fa17d55db3248c949fcbf5d6b70cbbefc07dba94
Abdulrahman <abdulrahmanali.h.t@gmail.com>
2026-09-04 13:04:59 +0000

    fix bug

--- biozone_web/www/cart.py
+++ biozone_web/www/cart.py
@@ -7,4 +7,5 @@ def get_context(context):
 	context.no_cache = 1
 	context.active_page = "cart"
 	context.update(get_header_context())
+	context.csrf_token = frappe.sessions.get_csrf_token()
 	return context
